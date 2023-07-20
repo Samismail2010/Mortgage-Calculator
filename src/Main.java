ca7ef7dc9b0a20a1a6d291c662ab8efb2077d3f6
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2023-07-20 02:02:42 -0500

    mortgage ques

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal: ");
+        System.out.print("Annual Interest Rate: ");
+        System.out.print("Period (Years): ");
+        System.out.print("Mortgage");
+
+
+
+    }
+}
